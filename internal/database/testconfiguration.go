35513e0df8737d5707539f88e8cbd4181936cb9c
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2024-01-24 21:11:11 +0100

    implement repository for testconfiguration

--- internal/database/testconfiguration.go
+++ internal/database/testconfiguration.go
@@ -7,3 +7,34 @@ type TestConfiguration struct {
 	ProductID     uint
 	Configuration map[string]string `gorm:"not null;serializer:json"`
 }
+
+type TestConfigurationRepository struct {
+	Database *gorm.DB
+}
+
+func NewTestConfigurationRepository(database *gorm.DB) *TestConfigurationRepository {
+	return &TestConfigurationRepository{
+		Database: database,
+	}
+}
+
+func (T *TestConfigurationRepository) InsertOne(productId uint, configuration map[string]string) *TestConfiguration {
+
+	testConfiguration := &TestConfiguration{
+		Model:         gorm.Model{},
+		ProductID:     productId,
+		Configuration: configuration,
+	}
+
+	T.Database.Debug().Create(testConfiguration)
+
+	return testConfiguration
+}
+
+func (T *TestConfigurationRepository) GetByProductIds(productIds []uint) []TestConfiguration {
+	var testConfigurations []TestConfiguration
+
+	T.Database.Debug().Where("product_id IN ?", productIds).Find(&testConfigurations)
+
+	return testConfigurations
+}
