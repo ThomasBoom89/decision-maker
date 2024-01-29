d45718594a0dffeef16d046838bd3cc244cd98b4
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2024-01-29 21:45:09 +0100

    initial parameter repository implementation

--- internal/database/parameter.go
+++ internal/database/parameter.go
@@ -10,3 +10,21 @@ type Parameter struct {
 	Comparer        string           `gorm:"not null"`
 	ParameterValues []ParameterValue `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
+
+type ParameterRepository struct {
+	database *gorm.DB
+}
+
+func NewParameterRepository(database *gorm.DB) *ParameterRepository {
+	return &ParameterRepository{database: database}
+}
+
+func (P *ParameterRepository) GetParameterByConfigurationId(id uint) ([]Parameter, error) {
+	var parameters []Parameter
+	err := P.database.Debug().Model(Parameter{}).Where("configuration_id = ?", id).Find(parameters).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return parameters, nil
+}
