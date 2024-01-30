841bde4c640a4e327272e3c43070b82b90a513c6
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2024-01-30 21:33:57 +0100

    add slash to public path for static assets

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'public'),
-        publicPath: ""
+        publicPath: "/"
     },
     module: {
         rules: [
