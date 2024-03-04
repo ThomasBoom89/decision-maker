18a89713faf67338e8228519667082e0410bb604
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2024-03-04 21:30:27 +0100

    Feat: add range compare type

--- internal/decision/maker.go
+++ internal/decision/maker.go
@@ -1,5 +1,7 @@
 package decision
 
+import "strings"
+
 type Maker struct {
 	stringCaster *StringCaster
 	comparer     *Comparer
@@ -30,27 +32,35 @@ func NewMakerForTestConfiguration() *Maker {
 }
 
 func (M *Maker) Decide(schmalue string, value string, compare Compare, parameterType string) bool {
-	// todo: maybe full iteration per config per parameter
-	switch parameterType {
-	case Integer:
-		compare1 := M.stringCaster.toInt(schmalue)
-		compare2 := M.stringCaster.toInt(value)
-		return M.comparer.CompareInt(compare1, compare2, compare)
-	case Float:
-		compare1 := M.stringCaster.toFloat(schmalue)
-		compare2 := M.stringCaster.toFloat(value)
-		return M.comparer.CompareFloat(compare1, compare2, compare)
-	case DateTime:
-		compare1 := M.stringCaster.toInt(schmalue)
-		compare2 := M.stringCaster.toInt(value)
-		return M.comparer.CompareInt(compare1, compare2, compare)
-	case String:
-		return M.comparer.CompareString(schmalue, value, compare)
-	case Boolean:
-		compare1 := M.stringCaster.toBool(schmalue)
-		compare2 := M.stringCaster.toBool(value)
-		return M.comparer.CompareBool(compare1, compare2, compare)
-	default:
-		return false
+	if compare == Range {
+		// 50$%@100
+		vals := strings.Split(value, "$%@")
+		schmals := strings.Split(schmalue, "$%@")
+		result1 := M.Decide(schmals[0], vals[0], GreaterEqual, parameterType)
+		result2 := M.Decide(schmals[1], vals[1], LowerEqual, parameterType)
+		return result1 == true && result2 == true
+	} else {
+		switch parameterType {
+		case Integer:
+			compare1 := M.stringCaster.toInt(schmalue)
+			compare2 := M.stringCaster.toInt(value)
+			return M.comparer.CompareInt(compare1, compare2, compare)
+		case Float:
+			compare1 := M.stringCaster.toFloat(schmalue)
+			compare2 := M.stringCaster.toFloat(value)
+			return M.comparer.CompareFloat(compare1, compare2, compare)
+		case DateTime:
+			compare1 := M.stringCaster.toInt(schmalue)
+			compare2 := M.stringCaster.toInt(value)
+			return M.comparer.CompareInt(compare1, compare2, compare)
+		case String:
+			return M.comparer.CompareString(schmalue, value, compare)
+		case Boolean:
+			compare1 := M.stringCaster.toBool(schmalue)
+			compare2 := M.stringCaster.toBool(value)
+			return M.comparer.CompareBool(compare1, compare2, compare)
+		default:
+			return false
+		}
 	}
 }
